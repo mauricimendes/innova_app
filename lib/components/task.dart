62c584d88b6c68133be236e0692af7114835ae63
mauricimendes <mauricimendes.14@gmail.com>
2023-02-11 23:39:31 -0300

    Create a animation to visibile description of task

--- lib/components/task.dart
+++ lib/components/task.dart
@@ -10,10 +10,25 @@ class Task extends StatefulWidget {
 
 class _TaskState extends State<Task> {
   bool _checked = false;
+  bool _open = false;
+  double _sizeTask = 62;
 
   handleSetChecked() {
     setState(() {
       _checked = !_checked;
+    });
+  }
+
+  handleOpenTask() {
+    const double sizeOpenTask = 128;
+
+    setState(() {
+      _sizeTask = _sizeTask == sizeOpenTask ? 62 : sizeOpenTask;
+    });
+    Future.delayed(const Duration(milliseconds: 200), () {
+      setState(() {
+        _open = !_open;
+      });
     });
   }
 
@@ -44,46 +59,64 @@ class _TaskState extends State<Task> {
         direction: DismissDirection.endToStart,
         onDismissed: (direction) => {print('teste')},
         key: Key(''),
-        child: Container(
+        child: AnimatedContainer(
           decoration: BoxDecoration(
               color: theme.container, borderRadius: BorderRadius.circular(8)),
-          height: 62,
+          height: _sizeTask,
           width: double.infinity,
+          duration: const Duration(milliseconds: 200),
           child: InkWell(
+            onDoubleTap: handleOpenTask,
             onLongPress: () => handleSetChecked(),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.check_circle,
-                          color: _checked ? theme.brand : theme.secondary,
-                        ),
-                        const SizedBox(
-                          width: 16,
-                        ),
-                        DefaultTextStyle(
-                            style: TextStyle(
-                              color: theme.primary,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.check_circle,
+                              color: _checked ? theme.brand : theme.secondary,
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            DefaultTextStyle(
+                                style: TextStyle(
+                                  color: theme.primary,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                child: const Text('lavar louças')),
+                          ]),
+                      DefaultTextStyle(
+                          style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.bold,
-                            ),
-                            child: const Text('lavar louças')),
-                      ]),
-                  DefaultTextStyle(
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: theme.green),
-                      child: const Text('fácil'))
-                ],
-              ),
+                              color: theme.green),
+                          child: const Text('fácil'))
+                    ],
+                  ),
+                ),
+                Visibility(
+                  visible: _open,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16, top: 8, right: 16),
+                    child: DefaultTextStyle(
+                        style: TextStyle(color: theme.secondary),
+                        child: const Text(
+                            'Aqui vai um texto grande que vai ser a descrição da terafa que sera realizada.')),
+                  ),
+                )
+              ],
             ),
           ),
         ),
